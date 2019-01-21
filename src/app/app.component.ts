9319f267a5e22717782286b86a47b51a75b9e551
Kavinda Senarathne <kavindas@99x.lk>
2019-01-22 05:06:18 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  currentdate = new Date();
+  startDate;
+  monthToDate() {
+   this.startDate = moment().format('YYYY-MM-01');
+   return this.startDate;
+  }
+  quarterToDate() {
+    this.startDate = moment().startOf('quarter').format('YYYY-MM-DD');
+    return this.startDate;
+  }
+  yearToDate() {
+    this.startDate = moment().startOf('year').format('YYYY-MM-DD');
+    return this.startDate;
+  }
 }
